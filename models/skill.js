cb63d0ed7aaaa01512b25e691c978e1189c62bf4
Jesus Montelongo <jissousklp@gmail.com>
2024-02-19 10:59:10 -0700

    created skills model file and skills view file

---
+++ models/skill.js
@@ -0,0 +1,9 @@
+module.exports = {}
+
+var skills = [
+    {skill: "JavaScript", expYears: 1, projects: 1},
+    {skill: "Java", expYears: 7, projects: 1},
+    {skill: "HTML", expYears: 1, projects: 1},
+    {skill: "CSS", expYears: 1, projects: 1},
+    {skill: "Python", expYears: 0, projects: 0}
+];
